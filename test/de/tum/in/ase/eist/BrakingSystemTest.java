3292281ae5cfb24972e409f6455f33b88610701d
helenjim49 <helenjim49@gmail.com>
2022-06-22 20:19:09 +0200

    second

--- test/de/tum/in/ase/eist/BrakingSystemTest.java
+++ test/de/tum/in/ase/eist/BrakingSystemTest.java
@@ -12,11 +12,8 @@ class BrakingSystemTest {
 
 	@TestSubject
 	private BrakingSystem brakingSystem = new BrakingSystem();
-	@Mock
+	@Mock(fieldName = "abs")
 	private ABS abs = new ABS();
-
-	@Mock
-	private Brake brake;
 
 	@Mock(fieldName = "distanceSensor")
 	private Sensor distanceSensor;
@@ -24,11 +21,25 @@ class BrakingSystemTest {
 	@Mock(fieldName = "tractionSensor")
 	private Sensor tractionSensor;
 
+	@Mock(fieldName = "speedSensor")
+	private Sensor speedSensor;
+
+	@Mock(fieldName = "brakingPedal")
+	private Sensor brakingPedal;
+
+	@Mock(fieldName = "brake")
+	private Brake brake;
+
 	@Test
 	void testBrakeProcess() {
 //		expects all the object in the calculateNextFrame and reply all the objects
 		expect(abs.shouldOverrideBrake()).andReturn(false);
 		replay(abs);
+		expect(speedSensor.sense()).andReturn(150.0);
+		replay(speedSensor);
+		expect(distanceSensor.sense()).andReturn(200.0);
+		replay(distanceSensor);
+		expect(brakingPedal.sense()).andReturn(0.5);
 		abs.shouldOverrideBrake();
 		brakingSystem.calculateNextFrame();
 		//		verify the brake mock
